479edbfe2742881c5cd7b031bc55883f5804b5c9
Abid <fmaabid@gmail.com>
2023-09-04 00:15:47 +0600

    wip

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div style="display: flex; align-items: center; justify-content: center">
+    <img
+      src="https://media.tenor.com/3HzVNNRa_CMAAAAd/hindustani-bhau-hbmaza.gif"
+      typeof="gif"
+    />
   </div>
 </template>
