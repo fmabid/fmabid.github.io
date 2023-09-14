5c6397f6b69f1cb11a15a082a2bd11a7da7dbcc4
Abid <fmaabid@gmail.com>
2023-09-14 23:56:31 +0600

    just texts

--- app.vue
+++ app.vue
@@ -1,8 +1,49 @@
 <template>
   <div style="display: flex; align-items: center; justify-content: center">
-    <img
-      src="https://media.tenor.com/3HzVNNRa_CMAAAAd/hindustani-bhau-hbmaza.gif"
-      typeof="gif"
-    />
+    <div style="margin-top: 30px">
+      <pre>
+        Hi There.
+        <p>
+          It's Abid. Atm, I am redesigning my site from scratch. <br />
+          It will look interactive soon. Thanks for visiting.
+        </p>
+      </pre>
+
+      <div style="margin-top: 30px">
+        <p>Meanwhile you can visit:</p>
+        <ul>
+          <li>
+            <a href="https://github.com/fmabid">https://github.com/fmabid</a>
+          </li>
+          <li>
+            <a href="https://medium.com/@fmabid">https://medium.com/@fmabid</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/fmabid"
+              >https://www.linkedin.com/in/fmabid</a
+            >
+          </li>
+          <li>
+            <a href="https://scholar.google.com/citations?user=OY-eWmgAAAAJ&hl"
+              >https://scholar.google.com/citations?user=OY-eWmgAAAAJ&hl</a
+            >
+          </li>
+        </ul>
+
+        <p>Projects:</p>
+        <ul>
+          <li>
+            <a href="https://www.npmjs.com/package/vue3-simple-dropzone"
+              >https://www.npmjs.com/package/vue3-simple-dropzone</a
+            >
+          </li>
+          <li>
+            <a href="https://fakegen.fmabid.com/"
+              >https://fakegen.fmabid.com/</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
