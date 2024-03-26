13a770d34ac6876c5a9bb8a18e659514e094b009
Abid <fmaabid@gmail.com>
2024-03-26 15:39:42 +0600

    refactor

--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,68 @@
 <template>
-  <div class="main-body mx-auto">
-    <div class="container mx-auto">
+  <div class="container mx-auto">
+    <div class="main-body mx-auto">
       <Intro />
-    </div>
 
-    <div class="space-y-6">
-      <p>
-        Working at
-        <NuxtLink
-          to="https://www.sender.net/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="!text-[#F75B11]"
-          >Sender</NuxtLink
-        >.
-      </p>
-      <p>
-        I am a Developer and open source enthusiast. I like TypeScript, Go and
-        enjoy creating frontend stuff that sometimes eases some extra jobs. My
-        few projects can be found
-        <NuxtLink to="/projects" class="text-blue-700">here</NuxtLink>.
-      </p>
+      <div class="space-y-6">
+        <p>
+          Working at
+          <NuxtLink
+            to="https://www.sender.net/"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="!text-[#F75B11]"
+            >Sender</NuxtLink
+          >.
+        </p>
+        <p>
+          I am a Developer and open source enthusiast. I like TypeScript, Go and
+          enjoy creating frontend stuff that sometimes eases some extra jobs. My
+          few projects can be found
+          <NuxtLink to="/projects" class="text-blue-700">here</NuxtLink>.
+        </p>
 
-      <p>
-        Othside of my profession, I travel a lot. Mostly like to go on a
-        motorbike ride. Sometimes I capture moments and share them on my
-        <NuxtLink to="/https://www.instagram.com/fm_abid/" class="text-blue-700"
-          >instagram</NuxtLink
-        >. I also like to write on Medium.
-      </p>
-    </div>
-
-    <div class="relative mt-10">
-      <p class="mb-4">Find me on:</p>
-
-      <div class="flex flex-wrap gap-6 text-sm">
-        <NuxtLink
-          v-for="link in smLinks"
-          :key="link.name"
-          :to="link.profile"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="flex items-center gap-1"
-        >
-          <component :is="link.icon" class="mb-1" />
-          {{ link.title }}
-        </NuxtLink>
+        <p>
+          Othside of my profession, I travel a lot. Mostly like to go on a
+          motorbike ride. Sometimes I capture moments and share them on my
+          <NuxtLink
+            to="/https://www.instagram.com/fm_abid/"
+            class="text-blue-700"
+            >instagram</NuxtLink
+          >. I also like to write on Medium.
+        </p>
       </div>
 
-      <p class="mt-10">
-        Or you can drop me an email at
-        <a href="mailto:fmaabid@gmail.com" class="text-blue-700"
-          >fmaabid@gmail.com</a
-        >
+      <div class="relative mt-10">
+        <p class="mb-4">Find me on:</p>
 
-        or ping me on
-        <NuxtLink to="https://www.linkedin.com/in/fmabid" class="text-blue-700"
-          >LinkedIn</NuxtLink
-        >.
-      </p>
+        <div class="flex flex-wrap gap-6 text-sm">
+          <NuxtLink
+            v-for="link in smLinks"
+            :key="link.name"
+            :to="link.profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            class="flex items-center gap-1"
+          >
+            <component :is="link.icon" class="mb-1" />
+            {{ link.title }}
+          </NuxtLink>
+        </div>
+
+        <p class="mt-10">
+          Or you can drop me an email at
+          <a href="mailto:fmaabid@gmail.com" class="text-blue-700"
+            >fmaabid@gmail.com</a
+          >
+
+          or ping me on
+          <NuxtLink
+            to="https://www.linkedin.com/in/fmabid"
+            class="text-blue-700"
+            >LinkedIn</NuxtLink
+          >.
+        </p>
+      </div>
     </div>
   </div>
 </template>
