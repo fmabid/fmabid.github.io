07f5b278529db354a030f85480eeabf22b59f0d6
Abid <fmaabid@gmail.com>
2024-03-26 23:31:12 +0600

    render by loop

--- pages/projects.vue
+++ pages/projects.vue
@@ -8,32 +8,22 @@
 
     <div class="mx-auto mt-20 lg:px-80">
       <div class="relative block">
-        <h4 class="mb-6 text-center text-xl font-bold">Personal projects</h4>
+        <h4 class="mb-6 text-center text-xl font-bold">Crafts</h4>
 
         <div class="mx-auto grid grid-flow-col text-slate-500">
           <NuxtLink
-            to="https://www.npmjs.com/package/vue3-simple-dropzone"
+            v-for="project in personalProjects"
+            :key="project.name"
+            :to="project.preview"
             class="group flex w-[350px] items-center gap-4 p-4 no-underline opacity-50 hover:opacity-100"
           >
-            <NpmIcon
-              class="i-logos-nuxt-icon text-3xl opacity-50 saturate-0 group-hover:opacity-100"
+            <component
+              :is="project.icon"
+              class="text-3xl opacity-50 saturate-0 group-hover:opacity-100"
             />
             <div class="relative">
-              <p>Vue3 Simple Dropzone</p>
-              <p class="text-xs">File uploader component vor Vue3.</p>
-            </div>
-          </NuxtLink>
-
-          <NuxtLink
-            to="https://fakegen.fmabid.com/"
-            class="group flex w-[350px] items-center gap-4 p-4 no-underline opacity-50 hover:opacity-100"
-          >
-            <NuxtDotjsIcon
-              class="i-logos-nuxt-icon text-3xl opacity-50 saturate-0 group-hover:opacity-100"
-            />
-            <div class="relative">
-              <p>Fakegen</p>
-              <p class="text-xs">Generate fake data.</p>
+              <p>{{ project.name }}</p>
+              <p class="text-xs">{{ project.description }}</p>
             </div>
           </NuxtLink>
         </div>
@@ -51,4 +41,21 @@
 
 <script lang="ts" setup>
 import { NuxtDotjsIcon, NpmIcon } from "vue3-simple-icons";
+
+const personalProjects = [
+  {
+    name: "Vue3 Simple Dropzone",
+    description: "File uploader component vor Vue3.",
+    icon: NpmIcon,
+    srcCode: "https://www.npmjs.com/package/vue3-simple-dropzone",
+    preview: "https://www.npmjs.com/package/vue3-simple-dropzone",
+  },
+  {
+    name: "Fakegen",
+    description: "Generate fake data.",
+    icon: NuxtDotjsIcon,
+    srcCode: "https://fakegen.fmabid.com/",
+    preview: "https://fakegen.fmabid.com/",
+  },
+];
 </script>
