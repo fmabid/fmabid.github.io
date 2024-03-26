377dbf1b4949c69aaa6b41c6098b1924e7cc77d7
Abid <fmaabid@gmail.com>
2024-03-26 13:25:46 +0600

    chare directory structure

---
+++ components/Intro.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="relative py-5">
+    <div class="flex items-end space-x-6">
+      <div class="block h-52 w-52 overflow-hidden rounded-full">
+        <img
+          src="/images/me.jpeg"
+          alt=""
+          srcset=""
+          class="rounded-full object-cover object-center"
+        />
+      </div>
+
+      <article>
+        <div class="relative">
+          <p class="text-xl">Hello, I'm</p>
+          <h1 class="bold text-[36px] font-medium">Md. Faysal Mahmud Abid</h1>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container mx-auto">
+    <Intro />
+  </div>
+  <div style="display: flex; align-items: center; justify-content: center">
+    <div style="margin-top: 30px">
+      <pre>
+        Hi There.
+        <p>
+          It's Abid. Atm, I am redesigning my site from scratch. <br />
+          It will look interactive soon. Thanks for visiting.
+        </p>
+      </pre>
+
+      <div style="margin-top: 30px">
+        <p>Meanwhile you can visit:</p>
+        <ul>
+          <li>
+            <a href="https://github.com/fmabid">https://github.com/fmabid</a>
+          </li>
+          <li>
+            <a href="https://medium.com/@fmabid">https://medium.com/@fmabid</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/fmabid"
+              >https://www.linkedin.com/in/fmabid</a
+            >
+          </li>
+          <li>
+            <a href="https://scholar.google.com/citations?user=OY-eWmgAAAAJ&hl"
+              >https://scholar.google.com/citations?user=OY-eWmgAAAAJ&hl</a
+            >
+          </li>
+        </ul>
+
+        <p>Projects:</p>
+        <ul>
+          <li>
+            <a href="https://www.npmjs.com/package/vue3-simple-dropzone"
+              >https://www.npmjs.com/package/vue3-simple-dropzone</a
+            >
+          </li>
+          <li>
+            <a href="https://fakegen.fmabid.com/"
+              >https://fakegen.fmabid.com/</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
