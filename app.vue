b6a0bd8ad7f41e6bf31c8f76da1cf08c73406082
Abid <fmaabid@gmail.com>
2024-03-27 00:00:18 +0600

    projects and nav

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
+    <header class="relative">
+      <nav class="container mx-auto">
+        <ul class="flex items-center justify-end gap-x-5 py-4">
+          <li class="transition duration-150 ease-in-out hover:text-gray-600">
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li class="transition duration-150 ease-in-out hover:text-gray-600">
+            <NuxtLink to="/projects">Projects</NuxtLink>
+          </li>
+          <!-- <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li> -->
+        </ul>
+      </nav>
+    </header>
+
     <NuxtPage />
   </div>
 </template>
--- pages/projects.vue
+++ pages/projects.vue
@@ -15,11 +15,36 @@
             v-for="project in personalProjects"
             :key="project.name"
             :to="project.preview"
-            class="group flex w-[350px] items-center gap-4 p-4 no-underline opacity-50 hover:opacity-100"
+            class="group flex w-[350px] items-center gap-4 p-4 no-underline opacity-50 transition duration-300 ease-in-out hover:opacity-100"
           >
             <component
               :is="project.icon"
-              class="text-3xl opacity-50 saturate-0 group-hover:opacity-100"
+              class="text-3xl opacity-50 saturate-0 transition duration-300 ease-in-out group-hover:opacity-100"
+            />
+            <div class="relative">
+              <p>{{ project.name }}</p>
+              <p class="text-xs">{{ project.description }}</p>
+            </div>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+
+    <div class="mx-auto mt-20 lg:px-80">
+      <div class="relative block">
+        <h4 class="mb-6 text-center text-xl font-bold">Client Projects</h4>
+
+        <div class="mx-auto grid grid-flow-col text-slate-500">
+          <NuxtLink
+            v-for="project in clientProjects"
+            :key="project.name"
+            :to="project.preview"
+            target="_blank"
+            class="group flex w-[350px] items-center gap-4 p-4 no-underline opacity-50 transition duration-300 ease-in-out hover:opacity-100"
+          >
+            <component
+              :is="project.icon"
+              class="text-3xl opacity-50 saturate-0 transition duration-300 ease-in-out group-hover:opacity-100"
             />
             <div class="relative">
               <p>{{ project.name }}</p>
@@ -31,16 +56,21 @@
     </div>
   </div>
 
-  <div class="absolute bottom-0 text-xs">
-    This page is inspired by Anthony Fu's
-    <NuxtLink to="https://antfu.me/projects" class="font-bold text-blue-700"
-      >projects</NuxtLink
-    >.
+  <div class="mt-40 text-xs">
+    <div class="container mx-auto">
+      This page is inspired by Anthony Fu's
+      <NuxtLink
+        to="https://antfu.me/projects"
+        class="font-bold text-blue-700"
+        target="_blank"
+        >projects</NuxtLink
+      >.
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { NuxtDotjsIcon, NpmIcon } from "vue3-simple-icons";
+import { NuxtDotjsIcon, NpmIcon, VueDotjsIcon } from "vue3-simple-icons";
 
 const personalProjects = [
   {
@@ -56,6 +86,21 @@ const personalProjects = [
     icon: NuxtDotjsIcon,
     srcCode: "https://fakegen.fmabid.com/",
     preview: "https://fakegen.fmabid.com/",
+  },
+];
+
+const clientProjects = [
+  {
+    name: "Skrollo",
+    description: "Meme sharing platform.",
+    icon: NuxtDotjsIcon,
+    preview: "https://skrollo.com/",
+  },
+  {
+    name: "Shopemaa Dashboard",
+    description: "Admin dashboard for Shopemaa.",
+    icon: VueDotjsIcon,
+    preview: "https://shopemaa.com/",
   },
 ];
 </script>
